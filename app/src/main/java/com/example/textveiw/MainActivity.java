22c92c7a64d0a25dc5a3db688bf2d6b1caed4636
Nothing Nothing <nothing@nothing>
2025-01-12 15:06:52 +0200

    the text changing when i press the button

--- app/src/main/java/com/example/textveiw/MainActivity.java
+++ app/src/main/java/com/example/textveiw/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.textveiw;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,10 +14,24 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btn;
+    TextView tv;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    @SuppressLint("SetTextI18n")
+    public void OnPush(View view) {
+        btn = findViewById(R.id.btn);
+        tv = findViewById(R.id.tv);
+
+        tv.setText("Oh, yea, I’ve been clicked!");
+
+
 
     }
 }
